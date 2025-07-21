c7b609a83215cce411b265a9379c356fd0f83f8a
Noah Wong <noahwong@Mac.austin.rr.com>
2025-07-20 23:39:37 -0500

    Mongo Debugging

--- index.js
+++ index.js
@@ -17,12 +17,15 @@ app.use(cors({
 app.use(express.json());
 app.use('/', router);
 
-// Try mongoose connection
+// Mongoose connection establish
 mongoose.connect(mongo_uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 }).then(() => {
 	console.log('MongoDB connected');
+	app.listen(port, () => {
+		console.log(`Server listening on port ${port}`);
+	});
 }).catch(err => {
 	console.error('MongoDB connection error:', err);
 });
